039be8027e5527437a3f6f28cf18c6fc3c669092
romkravets <romann.kravets@gmail.com>
2019-08-04 13:22:05 +0300

    Added page contact

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import About from './compoments/about/About';
+import Home from './compoments/page/home/Home';
+import About from './compoments/page/about/About';
+import Contact from './compoments/page/contact/Contact';
+import Header from './compoments/header/Header';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.scss';
@@ -11,74 +14,46 @@ function App() {
         <Router>
           <div>
             <Header />
-
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
-            <Route path="/topics" component={Topics} />
+            <Route path="/contact" component={Contact} />
+            {/* //<Route path="/topics" component={Topics} /> */}
           </div>
       </Router>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
       </header>
     </div>
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
+// function Topic({ match }) {
+//   return <h3>Requested Param: {match.params.id}</h3>;
+// }
 
-function Topic({ match }) {
-  return <h3>Requested Param: {match.params.id}</h3>;
-}
+// function Topics({ match }) {
+//   return (
+//     <div>
+//       <h2>Topics</h2>
 
-function Topics({ match }) {
-  return (
-    <div>
-      <h2>Topics</h2>
+//       <ul>
+//         <li>
+//           <Link to={`${match.url}/components`}>Components</Link>
+//         </li>
+//         <li>
+//           <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+//         </li>
+//       </ul>
 
-      <ul>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-        </li>
-      </ul>
+//       <Route path={`${match.path}/:id`} component={Topic} />
+//       <Route
+//         exact
+//         path={match.path}
+//         render={() => <h3>Please select a topic.</h3>}
+//       />
+//     </div>
+//   );
+// }
 
-      <Route path={`${match.path}/:id`} component={Topic} />
-      <Route
-        exact
-        path={match.path}
-        render={() => <h3>Please select a topic.</h3>}
-      />
-    </div>
-  );
-}
 
-function Header() {
-  return (
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/topics">Topics</Link>
-      </li>
-    </ul>
-  );
-}
 
 export default App;

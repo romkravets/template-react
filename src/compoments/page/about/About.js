e9381ef9a4c54676db640c40d2b5d7a4612de4e2
romkravets <romann.kravets@gmail.com>
2019-08-11 20:18:51 +0300

    Added SimpleSlider

--- src/compoments/page/about/About.js
+++ src/compoments/page/about/About.js
@@ -1,7 +1,15 @@
 import React from 'react';
-
-function About() {
-   return <h2>About</h2>;
+import SimpleSlider from '../../slider/Slider';
+export class About extends React.Component {
+  render() {
+      return (
+        <div>
+          <h2>About</h2>
+          <div>
+            <SimpleSlider/>
+          </div>
+      </div>
+    )
+  }
  }
-
  export default About;
